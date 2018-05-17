10bb0237beb9f4e2f7dbe5056591ae16c4241f86
Michael Stebel <mdstebel@gmail.com>
2018-05-17 16:39:07 -0400

    Initial Commit

---
+++ MDSComplexNumbers/MDSComplexNumbers.h
@@ -0,0 +1,19 @@
+//
+//  MDSComplexNumbers.h
+//  MDSComplexNumbers
+//
+//  Created by Michael Stebel on 5/17/18.
+//  Copyright © 2018 Michael Stebel Consulting, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MDSComplexNumbers.
+FOUNDATION_EXPORT double MDSComplexNumbersVersionNumber;
+
+//! Project version string for MDSComplexNumbers.
+FOUNDATION_EXPORT const unsigned char MDSComplexNumbersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDSComplexNumbers/PublicHeader.h>
+
+
